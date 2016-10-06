4e0356b37bea9badd8d424dad831ed7180e2471e
Paul McCarthy <paulmccarthy2010@live.ca>
2016-10-06 22:12:10 +0000

    working

--- callbackintro.js
+++ callbackintro.js
@@ -1,5 +1,5 @@
 function findWaldo(arr, found) {
-  arr.forEach(function(name, index) {
+  arr.forEach(function(name, index) { //for.each is a function
   //goes through list and calls upon it.
     if (name == "Waldo") {
     found(index);
@@ -9,11 +9,11 @@ function findWaldo(arr, found) {
 
 //Index value is passed and printing
 function actionWhenFound(all) {
-  console.log("Found him at: " + all + ".");
+  console.log("Found him at: " + all + ".");// all is a closure.
   //Seen something similar in ruby and looked on stackoverflow which put me in the right direction.
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 
-//I still need to doe more reading on the callback functions. A little confused right now but myslef and Lydia got through it.
+//I still need to doe more reading on the callback functions. A little confused right now but myseLf and Lydia got through it.
 
