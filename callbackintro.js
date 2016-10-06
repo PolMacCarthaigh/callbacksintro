bf247e03881d5db448e0d48afdea7ca068b9a4ef
Paul McCarthy <paulmccarthy2010@live.ca>
2016-10-06 19:10:43 +0000

    Found Waldo

---
+++ callbackintro.js
@@ -0,0 +1,19 @@
+function findWaldo(arr, found) {
+  arr.forEach(function(name, index) {
+  //goes through list and calls upon it.
+    if (name == "Waldo") {
+    found(index);
+    }
+  });
+}
+
+//Index value is passed and printing
+function actionWhenFound(index) {
+  console.log(`Found him at ${index}`);
+  //Seen something similar in ruby and looked on stackoverflow which put me in the right direction.
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+//I still need to doe more reading on the callback functions. A little confused right now but myslef and Lydia got through it.
+
